063800a219db1c1342b6fdcc35d1ae4bfc05cabc
yuwu <wuyumail>
2018-11-09 09:55:41 +0800

    处理玩家死亡

--- src/main/java/com/avoole/cells/handler/PlayerDeathMessageHandler.java
+++ src/main/java/com/avoole/cells/handler/PlayerDeathMessageHandler.java
@@ -3,7 +3,12 @@ package com.avoole.cells.handler;
 
 import com.avoole.cells.Client;
 import com.avoole.cells.data.Message;
+import com.avoole.cells.data.Player;
 import com.avoole.cells.storage.WorldStore;
+import com.avoole.cells.util.MessageUtil;
+import io.netty.handler.codec.http.websocketx.WebSocketFrame;
+
+import java.util.List;
 
 public class PlayerDeathMessageHandler implements MessageHandler {
 
@@ -18,6 +23,21 @@ public class PlayerDeathMessageHandler implements MessageHandler {
         Client client = message.getClient();
         if (client == null || !client.isConnected()) {
             return;
+        }
+
+        Player targetPlayer = (Player)message.getPayload();
+        if(!store.hasPlayer(targetPlayer)){
+            return;
+        }
+        store.removePlayer(targetPlayer);
+
+        Message newMessage = new Message();
+        newMessage.setPayload(targetPlayer);
+
+        WebSocketFrame frame = MessageUtil.getMessagePlayerDeath(newMessage);
+        List<Player> players = store.getPlayers();
+        for(Player player : players){
+            player.getClient().getCtx().writeAndFlush(frame);
         }
     }
 }
