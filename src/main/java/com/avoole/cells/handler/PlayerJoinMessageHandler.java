740993d3796a1d8e09414f58a4a3a3dd2ec36bb4
yuwu <wuyumail>
2018-11-09 09:24:07 +0800

    处理玩家加入

--- src/main/java/com/avoole/cells/handler/PlayerJoinMessageHandler.java
+++ src/main/java/com/avoole/cells/handler/PlayerJoinMessageHandler.java
@@ -2,13 +2,16 @@ package com.avoole.cells.handler;
 
 
 import com.avoole.cells.Client;
+import com.avoole.cells.data.Cell;
 import com.avoole.cells.data.Message;
 import com.avoole.cells.data.Player;
 import com.avoole.cells.storage.WorldStore;
 import com.avoole.cells.util.MessageUtil;
 import io.netty.handler.codec.http.websocketx.WebSocketFrame;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class PlayerJoinMessageHandler implements MessageHandler {
 
@@ -25,12 +28,26 @@ public class PlayerJoinMessageHandler implements MessageHandler {
             return;
         }
 
-        Player player = store.newPlayer();
+        Player targetPlayer = (Player)message.getPayload();
+        store.addPlayer(targetPlayer);
+
+        List<Cell> cells = store.getCells();
         List<Player> players = store.getPlayers();
+        int width = store.getWidth();
+        int hegiht = store.getHeight();
 
+        Map<String, Object> payload = new HashMap<>();
+        payload.put("width", width);
+        payload.put("hegiht", hegiht);
+        payload.put("cells", cells);
+        payload.put("players", players);
 
+        Message newMessage = new Message();
+        newMessage.setPayload(payload);
 
         WebSocketFrame frame = MessageUtil.getMessageWorld(message);
-        message.getClient().getCtx().writeAndFlush(frame);
+        for(Player player : players){
+            player.getClient().getCtx().writeAndFlush(frame);
+        }
     }
 }
